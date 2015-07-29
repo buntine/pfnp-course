429c1993cc7ea4e68999602058340576a7fa5b4a
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-07-29 19:59:28 +1000

    Added course viewer route

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 #[macro_use]
 extern crate nickel;
 
-use nickel::{Nickel, StaticFilesHandler};
+use nickel::{Nickel, HttpRouter, StaticFilesHandler};
 
 fn main() {
     let mut server = Nickel::new();
+    let mut router = Nickel::router();
 
-    server.utilize(router! {
-        get "/" =>  |_req, _res| {
-            "Hello, world"
-        }
+    router.get("/course/:course", middleware! { |req|
+        format!("Course {}", req.param("course").unwrap())
     });
 
+    server.utilize(router);
     server.utilize(StaticFilesHandler::new("assets/"));
 
     server.listen("127.0.0.1:6767");
