93c233ae95bf677fd408312f1209f6c284294d33
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-07-29 20:19:53 +1000

    Added course view

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 #[macro_use]
 extern crate nickel;
 
+use std::collections::HashMap;
 use nickel::{Nickel, HttpRouter, StaticFilesHandler};
 
 fn main() {
     let mut server = Nickel::new();
     let mut router = Nickel::router();
 
-    router.get("/course/:course", middleware! { |req|
-        format!("Course {}", req.param("course").unwrap())
+    router.get("/course/:course", middleware! { |req, res|
+        let mut data = HashMap::new();
+
+        data.insert("course", req.param("course").unwrap());
+
+        return res.render("views/course.tpl", &data);
     });
 
     server.utilize(router);
---
+++ views/course.tpl
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h1>{{course}}</h1>
+</body>
+</html>
