aa981cfc36d3accc5664c455b78c6bc28d57f7af
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-07-29 19:51:41 +1000

    Added static file serving

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 #[macro_use]
 extern crate nickel;
 
-use nickel::Nickel;
+use nickel::{Nickel, StaticFilesHandler};
 
 fn main() {
     let mut server = Nickel::new();
 
     server.utilize(router! {
-        get "**" =>  |_req, _res| {
+        get "/" =>  |_req, _res| {
             "Hello, world"
         }
     });
+
+    server.utilize(StaticFilesHandler::new("assets/"));
 
     server.listen("127.0.0.1:6767");
 }
