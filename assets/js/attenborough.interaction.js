6c0d9ed148d969fdc72cdee4c30c6eb29df05b97
Andrew Buntine von Doom <info@bunts.io>
2015-08-14 16:57:55 +1000

    Creating and removing canvas

--- assets/js/attenborough.interaction.js
+++ assets/js/attenborough.interaction.js
@@ -1,9 +1,23 @@
 (function(){
+  var createCanvas = function(id){
+    var $cvs = $("<canvas>", {id: id});
+
+    $("body").append($cvs);
+
+    return $("body").find("#" + id)[0];
+  };
+
   var attenborough = function(){
         this.up = function(){
+          var cvs = createCanvas("attenborough_cvs"),
+              ctx = cvs.getContext("2d");
+ 
+          cvs.width = window.innerWidth;
+          cvs.height = window.innerHeight;
         };
 
         this.down = function(){
+          $("#attenborough_cvs").remove();
         };
       };
 
