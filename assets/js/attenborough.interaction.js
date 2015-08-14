818754a03e032328355de6ca96bd608877c1a297
Andrew Buntine von Doom <info@bunts.io>
2015-08-14 17:23:33 +1000

    Setup canvas to draw David haha

--- assets/js/attenborough.interaction.js
+++ assets/js/attenborough.interaction.js
@@ -1,25 +1,30 @@
 (function(){
-  var createCanvas = function(id){
-    var $cvs = $("<canvas>", {id: id});
+  var attenborough,
+      david = new Image(),
+      createCanvas = function(id){
+        var $cvs = $("<canvas>", {id: id});
 
-    $("body").append($cvs);
+        $("body").append($cvs);
 
-    return $("body").find("#" + id)[0];
-  };
-
-  var attenborough = function(){
-        this.up = function(){
-          var cvs = createCanvas("attenborough_cvs"),
-              ctx = cvs.getContext("2d");
- 
-          cvs.width = window.innerWidth;
-          cvs.height = window.innerHeight;
-        };
-
-        this.down = function(){
-          $("#attenborough_cvs").remove();
-        };
+        return $("body").find("#" + id)[0];
       };
+
+  david.src = "/images/attenborough_cutout_1.png";
+
+  attenborough = function(){
+    this.up = function(){
+      var cvs = createCanvas("attenborough_cvs"),
+          ctx = cvs.getContext("2d");
+      cvs.width = window.innerWidth;
+      cvs.height = window.innerHeight;
+
+      ctx.drawImage(david, 10, 10);
+    };
+
+    this.down = function(){
+      $("#attenborough_cvs").remove();
+    };
+  };
 
   window.Attenborough = attenborough;
 })();
