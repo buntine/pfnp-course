21417646ed2a728a72235afa2b6811e83a1d031f
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-08-14 21:11:19 +1000

    Animating David like a boss

--- assets/js/attenborough.interaction.js
+++ assets/js/attenborough.interaction.js
@@ -1,27 +1,46 @@
 (function(){
   var attenborough,
-      david = new Image(),
+      animFrame,
+      cvs,
+      ctx,
+      david = {
+        pic: new Image(),
+      },
       createCanvas = function(id){
         var $cvs = $("<canvas>", {id: id});
 
         $("body").append($cvs);
 
         return $("body").find("#" + id)[0];
+      },
+      positionPic = function(){
+        david.x += Math.random() * 8;
+        david.y += Math.random() * 3;
+
+        ctx.clearRect(0, 0, cvs.width, cvs.height);
+        ctx.drawImage(david.pic, david.x, david.y);
+
+        animFrame = requestAnimationFrame(positionPic);
       };
 
-  david.src = "/images/attenborough_cutout_1.png";
+  david.pic.src = "/images/attenborough_cutout_1.png";
 
   attenborough = function(){
     this.up = function(){
-      var cvs = createCanvas("attenborough_cvs"),
-          ctx = cvs.getContext("2d");
+      david.x = 0;
+      david.y = 0;
+
+      cvs = createCanvas("attenborough_cvs");
+      ctx = cvs.getContext("2d");
+
       cvs.width = window.innerWidth;
       cvs.height = window.innerHeight;
 
-      ctx.drawImage(david, 10, 10);
+      animFrame = requestAnimationFrame(positionPic);
     };
 
     this.down = function(){
+      cancelAnimationFrame(animFrame);
       $("#attenborough_cvs").remove();
     };
   };
