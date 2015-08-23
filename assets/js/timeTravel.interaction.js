85382307fbb94277adb625a20f063c727f9372be
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-08-23 11:26:11 +1000

    Adding time travel interaction

--- assets/js/timeTravel.interaction.js
+++ assets/js/timeTravel.interaction.js
@@ -1,9 +1,18 @@
 (function(){
-  var timeTravel = function(){
+  var song = new Audio("/sounds/great_scott.mp3"),
+      timeTravel = function(){
         this.up = function(){
+          $("body").prepend("<img src=\"/images/time_travel.gif\" class=\"history_gif\">");
+          $("h1").css("z-index", 110);
+
+          song.play();
         };
 
         this.down = function(){
+          $(".history_gif").remove();
+
+          song.pause();
+          song.currentTime = 0;
         };
       };
 
